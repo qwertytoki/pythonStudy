0078bd1a1ed159040bcc4cb8bd9bf34daeb590ce
qwertytoki <qwertytoki@gmail.com>
2019-02-20 16:47:16 +0800

    learn "if"

--- hello.py
+++ hello.py
@@ -1,14 +1,18 @@
 
 def call_hello():
     print("hello")
-    
     for num in range(5):
         print(num)
     fruitList = ["apple","banana","grape"]
     for fruit in fruitList:
         print(fruit)
-
-
+    number = 2
+    if number ==3:
+        print("3dayo")
+    elif number ==2:
+        print("2dayo")
+    else:
+        print("3じゃないよ")
 
 def call_hoge():
     print("hoge")
