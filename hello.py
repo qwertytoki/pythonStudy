00d3df4c05514efd557d0c93af1958d582dad025
qwertytoki <qwertytoki@gmail.com>
2019-02-20 16:54:01 +0800

    learn import other module and gitignore

--- hello.py
+++ hello.py
@@ -1,22 +1,4 @@
-
-def call_hello():
-    print("hello")
-    for num in range(5):
-        print(num)
-    fruitList = ["apple","banana","grape"]
-    for fruit in fruitList:
-        print(fruit)
-    number = 2
-    if number ==3:
-        print("3dayo")
-    elif number ==2:
-        print("2dayo")
-    else:
-        print("3じゃないよ")
-
-def call_hoge():
-    print("hoge")
-
+import hello_caller
 
 if __name__ == "__main__":
-    call_hello()
+    hello_caller.call_hello()
