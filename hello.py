9f00e7cd4f6ca426d7cc6b2e8de1c91e95999c39
qwertytoki <qwertytoki@gmail.com>
2019-02-20 16:43:25 +0800

    learn "for"

--- hello.py
+++ hello.py
@@ -2,6 +2,17 @@
 def call_hello():
     print("hello")
     
+    for num in range(5):
+        print(num)
+    fruitList = ["apple","banana","grape"]
+    for fruit in fruitList:
+        print(fruit)
+
+
+
+def call_hoge():
+    print("hoge")
+
 
 if __name__ == "__main__":
     call_hello()
